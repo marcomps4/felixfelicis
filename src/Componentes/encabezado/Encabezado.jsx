6754ba9016f7ccbfda3caeb0fd7854d916a44390
Marco <mavelandiag74@gmail.com>
2024-10-27 17:00:09 -0500

    se agrega imagen de header final y se arreglan tamaños de fuentes

--- src/Componentes/encabezado/Encabezado.jsx
+++ src/Componentes/encabezado/Encabezado.jsx
@@ -1,7 +1,5 @@
 import '../encabezado/encabezado.css';
-import bg1 from '../../Assets/encabezado/01_fondo.jpg';
-import manos from '../../Assets/encabezado/01_recorte.png';
-import vector from '../../Assets/encabezado/deco_03.png';
+import fondo from '../../Assets/encabezado/fondo.png';
 
 
 
@@ -9,9 +7,8 @@ const Encabezado = () => {
     return(
 
             <div className='encabezado'>
-                <img src={bg1} alt="fondo1" className='bg'/>
-                <img src={manos} alt="fondo1" className='hands'/>
-                <img src={vector} alt="fondo1" className='vector'/>
+                <img src={fondo} alt="imagen de fondo" className='fondo_img' />
+
                 <div className="container">
                 <div className='title'>
                     <h1>Catalina & Jhonatan</h1>
